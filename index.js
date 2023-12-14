fa23b88502194d79d36a09d034f9d551b2922ec7
Arka Mukherjee <85149698+Rendro1@users.noreply.github.com>
2023-12-14 22:02:51 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData ={
+        "login": "octocat",
+        "id": 1,
+        "node_id": "MDQ6VXNlcjE=",
+        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/octocat",
+        "html_url": "https://github.com/octocat",
+        "followers_url": "https://api.github.com/users/octocat/followers",
+        "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+        "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+        "organizations_url": "https://api.github.com/users/octocat/orgs",
+        "repos_url": "https://api.github.com/users/octocat/repos",
+        "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/octocat/received_events",
+        "type": "User",
+        "site_admin": false
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -12,6 +33,10 @@ app.get('/twitter', (req, res) => {
 })
 app.get('/login', (req, res) => {
   res.send('<h1>Login SuccessFull!<h1/>')
+})
+
+app.get('/github',(req,res)=>{
+    res.json(githubData)
 })
 
 
